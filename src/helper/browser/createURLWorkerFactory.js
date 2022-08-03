c03e1ebcdde5d6bf90aa9ec404672997c50d5dff
Matt Rossman <matt.rossman@quarkworks.co>
2022-08-03 16:57:58 -0400

    Support dynamicBasePath

--- src/helper/browser/createURLWorkerFactory.js
+++ src/helper/browser/createURLWorkerFactory.js
@@ -1,5 +1,14 @@
-export function createURLWorkerFactory(url) {
-    return function WorkerFactory(options) {
-        return new Worker(url, options);
+export function createURLWorkerFactory(url, basePath) {
+    return function WorkerFactory(options, basePathOverride) {
+        let workerUrl;
+        try {
+            workerUrl = new URL(url, basePathOverride ?? basePath).toString();
+        } catch (e) {
+            workerUrl = new URL(
+                (basePathOverride ?? basePath) + "/" + url,
+                window.location.origin
+            ).toString();
+        }
+        return new Worker(workerUrl, options);
     };
 }
